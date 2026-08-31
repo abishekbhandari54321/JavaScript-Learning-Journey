a7a3aac35f4ac0a3c305b8eca661b673521c3933
Abishek Bhandari <abishekbhandari54321@gmail.com>
2026-08-31 18:24:21 +0530

    Add Day 5 JavaScript Learning

---
+++ Day-05/Day-05.js
@@ -0,0 +1,66 @@
+//Function in JavaScript:
+function greet(){
+    console.log("Hello");
+}
+greet();
+
+console.log("...............................");
+
+//real usecases:
+function showWelcomeMessage(name){
+    console.log("Welcome, " + name + "!");
+}
+showWelcomeMessage("Abishek");
+showWelcomeMessage("John");     
+
+console.log("...............................");
+
+//Parameter and Argument:
+function person(name){
+    console.log("Hello, " + name);
+}
+person("Abishek");
+
+console.log("...............................");
+
+//return:
+function add(a,b){
+    return a+b;
+}
+let sum = add(5,10);
+console.log(sum);
+
+console.log("...............................");
+
+// Regular function
+function add(a, b) {
+    return a + b;
+}
+
+// Arrow function (same thing)
+const add2 = (a, b) => {
+    return a + b;
+};
+
+// Arrow function - shorter version (implicit return)
+const add3 = (a, b) => a + b;
+
+console.log(add(2, 3));   // 5
+console.log(add2(5, 4));  // 5
+console.log(add3(4, 4));  // 5
+
+console.log("...............................");
+
+// Multiple lines — needs { } and return
+const calculateTax = (price) => {
+    let tax = price * 0.1;
+    return tax;
+}
+console.log(calculateTax(100)); // 10
+
+// One line — implicit return works
+const double = (num) => num * 2;
+console.log(double(10)); // 20
+
+console.log("...............................");
+
